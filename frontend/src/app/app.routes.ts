282c8d43080da0758af6e97ebcfb35dc29b0abcd
paflov <paf@titelfrei.de>
2026-06-25 13:22:42 +0200

    Create basic project structure

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {About} from './components/pages/about/about';
+import {Status} from './components/pages/status/status';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'pages/about', component: About },
+  { path: 'pages/status', component: Status },
+];
