234d5ad48b42624b16a5e4f11a89cd66b94c9a2e
paflov <paf@titelfrei.de>
2026-06-25 13:54:41 +0200

    Connect to postgresql

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
-import type { Msg } from 'common/interfaces/default';
+import {Controller, Get} from '@nestjs/common';
+import {AppService} from './app.service';
+import type {Status} from 'common/interfaces/default';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): Msg {
-    return this.appService.getHello();
+  async getHello(): Promise<Status> {
+    return this.appService.getStatus();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@nestjs/common';
-import { Msg } from 'common/interfaces/default';
+import { Status} from 'common/interfaces/default';
+import { Client } from 'pg';
 
 @Injectable()
 export class AppService {
-  getHello(): Msg {
-    return {text: 'Hello World!'};
+  async getStatus(): Promise<Status> {
+
+    const client = new Client({
+      user: 'app_user',
+      password: 'secret_password',
+      host: 'localhost',
+      port: 5334,
+      database: 'app_db',
+    });
+
+
+    const eamon = await client.query('select * from meta');
+    console.log(eamon);
+
+    return {
+      app: 'concepts-backend',
+      db: {
+        status: 'unknown',
+        version: '0.0.0'
+      },
+      version: '0.0.0'
+    };
   }
 }
