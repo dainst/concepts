12eca40e16d4ab3ac4e687ffa467ee34310f6048
paflov <paf@titelfrei.de>
2026-09-03 17:47:40 +0200

    [FE] GraphView: fix expansion conditions

--- frontend/src/app/functions/graph-data.ts
+++ frontend/src/app/functions/graph-data.ts
@@ -1,7 +1,7 @@
 import {GraphLink, GraphNode} from '../interfaces/graph';
 import {ConceptId} from 'concepts-common/interfaces/concept';
 
-export const stringifyId = (cid: ConceptId|GraphNode): string => `${cid.id}«${cid.type}`;
+export const stringifyId = (cid: ConceptId|GraphNode): string => `${cid.type}/${cid.id}`;
 export const stringifyLinkId = (link: GraphLink): string => {
   const sV = (v: string|number|GraphNode|ConceptId): string => {
     switch (typeof v) {
@@ -10,8 +10,7 @@ export const stringifyLinkId = (link: GraphLink): string => {
       case "object": return stringifyId(v);
     }
   }
-  const objects = [sV(link.source), sV(link.target)].sort();
-  return `${sV(link.relation)}(${objects[0]},${objects[1]})`;
+  return `${sV(link.source)}|${sV(link.relation)}|${sV(link.target)}`;
 }
 
 
